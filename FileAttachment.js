7312bfcb9661f65e8c6d7c6deb77711327bcc650
Fanndís <fanndis@agrdynamics.com>
2020-04-29 13:29:08 +0000

    d3 test

--- FileAttachment.js
+++ FileAttachment.js
@@ -43,14 +43,4 @@ class FileAttachment {
   }
 }
 
-export function NoFileAttachments(name) {
-  throw new Error(`File not found: ${name}`);
-}
 
-export default function FileAttachments(resolve) {
-  return name => {
-    const url = resolve(name += ""); // Returns a Promise, string, or null.
-    if (url == null) throw new Error(`File not found: ${name}`);
-    return new FileAttachment(url, name);
-  };
-}
